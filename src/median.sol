e962ba4639c52a055eef035442a97f54ec66c89a
zaryab <zaryabbkh@gmail.com>
2023-05-26 12:51:09 +0500

    ETHGSU, USDTGSU medians added

--- src/median.sol
+++ src/median.sol
@@ -270,3 +270,25 @@ contract MedianAAVEUSD is Median {
         );
     }
 }
+
+contract MedianETHGSU is Median {
+    bytes32 public constant wat = "ETHGSU";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
+contract MedianUSDTGSU is Median {
+    bytes32 public constant wat = "USDTGSU";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
